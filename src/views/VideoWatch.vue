699412ab5015bde8af695519b14240d1234a3744
Jolka <firjola@gmail.com>
2024-06-07 14:59:37 +0200

    refactor : change html video to vue-video-player

--- src/views/VideoWatch.vue
+++ src/views/VideoWatch.vue
@@ -1,7 +1,13 @@
 <script>
 import { useVideosStore } from '@/stores/videos'
 
+import { VideoPlayer } from '@videojs-player/vue'
+import 'video.js/dist/video-js.css'
+
 export default {
+  components: {
+    VideoPlayer
+  },
   computed: {
     video() {
       return useVideosStore().videos.find((video) => video.id == this.$route.params.id)
@@ -10,18 +16,24 @@ export default {
 }
 </script>
 <template>
-  <video controls>
-    <source :src="video.videoUrl" type="video/mp4" />
-    <p>
-      Your browser doesn't support HTML video. Here is a
-      <a :href="video.videoUrl" download="video.videoUrl">link to the video</a> instead.
-    </p>
-  </video>
+  <video-player
+    class="video-watch"
+    :src="video.videoUrl"
+    :options="{ playbackRates: [1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0] }"
+    :poster="video.thumbnail"
+    controls
+    :loop="true"
+    :volume="0.6"
+  />
+
   <h1>{{ video.name }}</h1>
   <div v-html="video.description"></div>
 </template>
-<style scoped>
-img {
-  max-width: 50%;
+<style>
+.video-watch.video-js {
+  width: 50vw;
+  aspect-ratio: 2 / 1;
+  height: auto;
+  margin: auto;
 }
 </style>
