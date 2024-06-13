a1874f51c7a79e0830dbf633bc1293407fa2e9dd
Jolka <firjola@gmail.com>
2024-06-13 13:30:27 +0200

    cleanup : delete commented out block

--- src/server.js
+++ src/server.js
@@ -22,28 +22,3 @@ export function makeServer({ environment = 'development' } = {}) {
 
   return server
 }
-
-// import { createServer, Model, JSONAPISerializer, hasMany } from 'miragejs'
-// import videosJSON from '@/mirage/videos.json'
-// import tagsJSON from '@/mirage/tags.json'
-
-// export function makeServer({ environment = 'development' } = {}) {
-//   console.log('server')
-//   let server = createServer({
-//     environment,
-//     fixtures: { videos: videosJSON, tags : tagsJSON },
-//     models: { video: Model.extend({tags: hasMany()}),tag: Model.extend({videos: hasMany()}) },
-
-//     seeds(server) {},
-//     serializers: { application: JSONAPISerializer,
-//     video : JSONAPISerializer.extend({include : ['tags']}),
-//     tag : JSONAPISerializer.extend({include : ['videos']}),
-
-//     routes() {
-//       this.namespace = 'api'
-//       this.get('/videos')
-//     }
-//   })
-
-//   return server
-// }
