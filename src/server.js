4c57fa254a62970ccf74494656664292272cac60
Jolka <firjola@gmail.com>
2024-06-13 13:21:37 +0200

    feature : add relationships and tags for json data

--- src/server.js
+++ src/server.js
@@ -1,24 +1,49 @@
-import { createServer, Model } from 'miragejs'
+import { createServer, Model, JSONAPISerializer, hasMany } from 'miragejs'
 import videosJSON from '@/mirage/videos.json'
+import tagsJSON from '@/mirage/tags.json'
 
 export function makeServer({ environment = 'development' } = {}) {
   console.log('server')
   let server = createServer({
     environment,
-
-    models: {},
-
-    seeds(server) {},
-
+    fixtures: { videos: videosJSON, tags: tagsJSON },
+    models: { video: Model.extend({ tags: hasMany() }), tag: Model.extend({ videos: hasMany() }) },
     routes() {
       this.namespace = 'api'
-
-      this.get('/videos', ({ db }, request) => {
-        return db.videos
-      })
+      this.get('/videos')
+      this.get('/tags')
+    },
+    serializers: {
+      application: JSONAPISerializer,
+      video: JSONAPISerializer.extend({ include: ['tags'] }),
+      tag: JSONAPISerializer.extend({ include: ['videos'] })
     }
   })
-  server.db.loadData({ videos: videosJSON })
 
   return server
 }
+
+// import { createServer, Model, JSONAPISerializer, hasMany } from 'miragejs'
+// import videosJSON from '@/mirage/videos.json'
+// import tagsJSON from '@/mirage/tags.json'
+
+// export function makeServer({ environment = 'development' } = {}) {
+//   console.log('server')
+//   let server = createServer({
+//     environment,
+//     fixtures: { videos: videosJSON, tags : tagsJSON },
+//     models: { video: Model.extend({tags: hasMany()}),tag: Model.extend({videos: hasMany()}) },
+
+//     seeds(server) {},
+//     serializers: { application: JSONAPISerializer,
+//     video : JSONAPISerializer.extend({include : ['tags']}),
+//     tag : JSONAPISerializer.extend({include : ['videos']}),
+
+//     routes() {
+//       this.namespace = 'api'
+//       this.get('/videos')
+//     }
+//   })
+
+//   return server
+// }
