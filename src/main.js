a8bad3e75e661f316e0f1fee4d4d13f290938baf
Jolka <firjola@gmail.com>
2024-07-04 12:49:34 +0200

    config : installing icon fonts

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import { makeServer } from '@/server.js'
 import router from './router'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
 const vuetify = createVuetify({
   components,
