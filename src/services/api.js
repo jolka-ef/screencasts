e96e833ceeedd4ebf5d17f6e4b89797c4b9cefaa
Jolka <firjola@gmail.com>
2024-07-06 14:10:42 +0200

    add token auth

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,15 @@
 import axios from 'axios'
 
 export default () => {
+  let currentUser = JSON.parse(window.localStorage.currentUser)
+
   return axios.create({
     baseURL: '/api',
     withCredentials: false,
     headers: {
       Accept: 'application/json',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      Authorization: currentUser && currentUser.token
     }
   })
 }
